0f0a1c35782cdaaef1fa1b5dac65a94c7eedf461
james <james@jamess-MBP.home>
2022-09-30 17:48:33 +0100

    Original LSD to compile

--- src/ashift_lsd.c
+++ src/ashift_lsd.c
@@ -1216,16 +1216,19 @@ static double nfa(int n, int k, double p, double logNT)
 /*----------------------------------------------------------------------------*/
 /** Rectangle structure: line segment with width.
  */
-struct rect
-{
-  double x1,y1,x2,y2;  /* first and second point of the line segment */
-  double width;        /* rectangle width */
-  double x,y;          /* center of the rectangle */
-  double theta;        /* angle */
-  double dx,dy;        /* (dx,dy) is vector oriented as the line segment */
-  double prec;         /* tolerance angle */
-  double p;            /* probability of a point with angle within 'prec' */
-};
+
+#include "ashift.h"
+
+// struct rect
+// {
+//   double x1,y1,x2,y2;  /* first and second point of the line segment */
+//   double width;        /* rectangle width */
+//   double x,y;          /* center of the rectangle */
+//   double theta;        /* angle */
+//   double dx,dy;        /* (dx,dy) is vector oriented as the line segment */
+//   double prec;         /* tolerance angle */
+//   double p;            /* probability of a point with angle within 'prec' */
+// };
 
 /*----------------------------------------------------------------------------*/
 /** Copy one rectangle structure to another.
