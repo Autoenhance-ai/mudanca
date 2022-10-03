6ff030a99d7f54c73ff964ca0e2a4785db9bc922
james <james@jamess-MBP.home>
2022-10-03 09:43:07 +0100

    Clean up examples

--- examples/example.py
+++ examples/example.py
@@ -2,35 +2,21 @@ import cv2
 import pyshift
 import numpy as np
 
-# print(results)
-# print(results[1])
-# print(results[2])
-
-# lines = results[0]
-
-# img_with_lines = lsd.drawSegments(img, lines)
-
-img = cv2.imread('example.jpeg') 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-pyshift.adjust_lsd(img)
-
 img = cv2.imread('example.jpeg', 0)
 lsd = cv2.createLineSegmentDetector(0)
 
 lsd_results = lsd.detect(img)
 pyshift.adjust(img, lsd_results)
 
-# img_result = pyshift.adjust(img, lsd_results)
+# lines = results[0]
+# img_with_lines = lsd.drawSegments(img, lines)
 
+img = cv2.imread('example.jpeg') 
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+pyshift.adjust_lsd(img)
 
-
-# print("Result")
-# print(img_result)
-
-# img_result = correction.correct(lines, img)
 
 # M = cv2.getPerspectiveTransform(input_pts,output_pts)
-
 # out = cv2.warpPerspective(img,M,(img.shape[1], img.shape[0]),flags=cv2.INTER_LINEAR)
 
 # cv2.imshow("Original", img)
