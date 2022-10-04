7dcd55dca5eabb7ac59e60ae0306886b1670dba9
james <james@jamess-MBP.home>
2022-10-04 11:23:18 +0100

    MVP crop working

--- examples/example.py
+++ examples/example.py
@@ -12,20 +12,27 @@ print(cropbox)
 img = cv2.imread('example.jpeg')
 rows, cols, ddims = img.shape
 
-cl *= rows
-cr *= rows
-ct *= cols
-cb *= cols
+cl *= cols
+cr *= cols
+ct *= rows
+cb *= rows
 
 cl = int(cl)
 cr = int(cr)
 ct = int(ct)
 cb = int(cb)
 
-corrected_img = cv2.warpPerspective(img, matrix, (int(cols),int(rows)), flags=cv2.INTER_NEAREST)
-cropped_img = corrected_img[cl:cr, ct:cb]
+print(cl)
+print(cr)
+print(ct)
+print(cb)
 
-cv2.imwrite('fixed.jpeg', corrected_img)
+corrected_img = cv2.warpPerspective(img, matrix, (int(cols),int(rows)), flags=cv2.INTER_NEAREST)
+cropped_img = corrected_img[ct:cb, cl:cr]
+
+cv2.imwrite('corrected.jpeg', corrected_img)
+cv2.imwrite('cropped.jpeg', cropped_img)
+
 cv2.imshow("Original", img)
 cv2.imshow("Corrected", corrected_img)
 cv2.imshow("Cropped", cropped_img)
