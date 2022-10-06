f833fc0f6d5a7e16209baf0a7ab2b18cdd2c7395
james <james@jamess-MBP.home>
2022-10-06 12:15:03 +0100

    Cleaning memory

--- examples/example.py
+++ examples/example.py
@@ -5,6 +5,8 @@ import time
 img = cv2.imread('example.jpeg')
 iterations = 10
 
+print(img.dtype)
+
 for i in range(iterations):
     adjusted_img = pyshift.adjust(img)
-    cv2.imwrite(f'corrected-{i}.jpeg', adjusted_img)
+#     cv2.imwrite(f'corrected-{i}.jpeg', adjusted_img)
