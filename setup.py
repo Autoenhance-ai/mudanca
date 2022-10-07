5f8c6c721e2b26e0fd90693dc31d43e4b3e22364
james <james@jamess-MBP.home>
2022-10-07 15:55:58 +0100

    Graduate to 1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ module = Extension('pyshift', sources = [
 ])
 
 setup (name = 'Pyshift',
-       version = '0.2',
+       version = '1.0',
        description = 'An implementation of the shift algorithmn for python ported from Darktable & Nshift',
        ext_modules = cythonize([module], compiler_directives={'language_level' : "3"}),
        include_dirs=[numpy.get_include()],
