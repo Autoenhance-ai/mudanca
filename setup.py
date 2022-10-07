6a5ccc23cdeb3dd956a480da7e840139cfb63321
james <james@jamess-MBP.home>
2022-10-07 11:55:29 +0100

    Add LICENSE

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ module = Extension('pyshift', sources = [
 ])
 
 setup (name = 'Pyshift',
-       version = '1.0',
+       version = '0.1',
        description = 'An implementation of the shift algorithmn for python ported from Darktable & Nshift',
        ext_modules = cythonize([module], compiler_directives={'language_level' : "3"}),
        include_dirs=[numpy.get_include()],
