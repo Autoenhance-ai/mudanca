16d565d617757f42ae4bbe375860e369717820da
james <james@jamess-MBP.home>
2022-10-07 12:28:50 +0100

    Accept gray

--- examples/example.py
+++ examples/example.py
@@ -6,5 +6,18 @@ img = cv2.imread('example.jpeg')
 iterations = 10
 
 for i in range(iterations):
-    adjusted_img = pyshift.adjust(img)
+
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    matrix, cropbox = pyshift.adjust(grey)
+    
+    height, width, _ = img.shape
+    x1, y1 = cropbox[0]
+    x2, y2 = cropbox[1]
+
+    corrected_img = cv2.warpPerspective(img, matrix, (int(width), int(height)), flags=cv2.INTER_NEAREST)
+    adjusted_img = corrected_img[
+        x1:x2,
+        y1:y2
+    ]
+
     cv2.imwrite(f'corrected-{i}.jpeg', adjusted_img)
