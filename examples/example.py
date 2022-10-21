a994204403e9ad67856ee5298187033a34b7c4f5
james <james@jamess-MBP.home>
2022-10-21 15:31:59 +0100

    New example

--- examples/example.py
+++ examples/example.py
@@ -1,36 +1,29 @@
 import cv2
 import mudanca
+import numpy
 
 img = cv2.imread('example.jpeg')
-iterations = 10
 
-for i in range(iterations):
+lines = numpy.load(open('lines.npy', 'rb'))
 
-    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+height, width, _ = img.shape
 
-    lsd = cv2.createLineSegmentDetector(
-        cv2.LSD_REFINE_ST,
-        LSD_SCALE,
-        LSD_SIGMA_SCALE, 
-        LSD_QUANT,
-        LSD_ANG_TH,
-        LSD_LOG_EPS,
-        LSD_DENSITY_TH,
-        LSD_N_BINS
-    )
+lines /= 512
 
-    lines, _, _, _ = lsd.detect(img)
+lines[:,0] *= width
+lines[:,2] *= width
+lines[:,1] *= height
+lines[:,3] *= height
 
-    height, width, _ = img.shape
-    matrix, cropbox = mudanca.adjust(lines, (width, height), mudanca.FIT_VERTICALLY)
-    
-    x1, y1 = cropbox[0]
-    x2, y2 = cropbox[1]
+matrix, cropbox = mudanca.adjust(lines, (width, height), mudanca.FIT_VERTICALLY)
 
-    corrected_img = cv2.warpPerspective(img, matrix, (int(width), int(height)), flags=cv2.INTER_NEAREST)
-    adjusted_img = corrected_img[
-        x1:x2,
-        y1:y2
-    ]
+x1, y1 = cropbox[0]
+x2, y2 = cropbox[1]
 
-    cv2.imwrite(f'corrected-{i}.jpeg', adjusted_img)
+corrected_img = cv2.warpPerspective(img, matrix, (int(width), int(height)), flags=cv2.INTER_NEAREST)
+adjusted_img = corrected_img[
+    x1:x2,
+    y1:y2
+]
+
+cv2.imwrite(f'corrected.jpeg', adjusted_img)
